0ce9fe2b0333c397dea64ed05212c9b9aef486c4
afterglow <cotmdals0@naver.com>
2024-10-22 20:22:56 +0900

    mobile대응 완료, Animation 작업 요망

--- stdio.h/src/pages/Detail.jsx
+++ stdio.h/src/pages/Detail.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import './Detail.scss';
 
 import Navbar from "./Navbar.jsx";
--- stdio.h/src/pages/Home.jsx
+++ stdio.h/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-import MediaQuery, {useMediaQuery} from 'react-responsive'
 import './Home.scss';
 
 import Navbar from "./Navbar";
